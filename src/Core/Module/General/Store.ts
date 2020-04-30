b0f45d2491b3cb3f5386328621ae8f4b6f42088e
Martin Neundorfer <m.neundorfer@labor.digital>
2020-04-30 13:20:44 +0200

    feat(Store): make the store instance watchable

--- src/Core/Module/General/Store.ts
+++ src/Core/Module/General/Store.ts
@@ -16,16 +16,24 @@
  * Last modified: 2019.10.29 at 17:41
  */
 
+import {EventEmitter} from "@labor-digital/helferlein/lib/Events/EventEmitter";
+import {ListPath} from "@labor-digital/helferlein/lib/Interfaces/List";
 import {PlainObject} from "@labor-digital/helferlein/lib/Interfaces/PlainObject";
 import {forEach} from "@labor-digital/helferlein/lib/Lists/forEach";
 import {getPath} from "@labor-digital/helferlein/lib/Lists/Paths/getPath";
 import {hasPath} from "@labor-digital/helferlein/lib/Lists/Paths/hasPath";
-import {isArray} from "@labor-digital/helferlein/lib/Types/isArray";
 import {isFunction} from "@labor-digital/helferlein/lib/Types/isFunction";
 import {isPlainObject} from "@labor-digital/helferlein/lib/Types/isPlainObject";
-import {isString} from "@labor-digital/helferlein/lib/Types/isString";
 import {isUndefined} from "@labor-digital/helferlein/lib/Types/isUndefined";
 import Vue from "vue";
+
+export interface StoreWatcher {
+	(n: any, o: any): void
+}
+
+interface StoreVue extends Vue {
+	state: PlainObject;
+}
 
 /**
  * This class is a super simple state storage object.
@@ -34,12 +42,21 @@ import Vue from "vue";
 export class Store {
 	
 	/**
+	 * The event emitter to perform the binding proxy with
+	 */
+	protected _emitter: EventEmitter;
+	
+	/**
 	 * The internal, reactive storage object
 	 */
-	protected _store;
+	protected _store: StoreVue;
+	
+	/**
+	 * The list of already watched properties inside the store
+	 */
+	protected _watched: Set<string>;
 	
 	public constructor(internalInitialState: PlainObject, initialState ?: Function) {
-		
 		// Use a fallback to create an internal state if we did not got one provided
 		if (isUndefined(initialState)) initialState = () => {
 			return {};
@@ -52,16 +69,21 @@ export class Store {
 		const loadedState: PlainObject = initialState();
 		if (!isPlainObject(loadedState)) throw new Error("The given initial state function did not return a plain object!");
 		forEach(internalInitialState, (v, k) => isUndefined(loadedState[k]) ? loadedState[k] = v : null);
-		this._store = Vue.observable(loadedState);
+		this._store = new Vue({
+			data: {state: loadedState}
+		});
+		
+		// Create the event emitter instance and the list of watched properties
+		this._emitter = new EventEmitter(true);
+		this._watched = new Set<string>();
 	}
 	
 	/**
 	 * Returns true if the store has a value for a given key
 	 * @param key
 	 */
-	public has(key: string | Array<string>): boolean {
-		if (isArray(key) || isString(key) && key.indexOf(".") !== -1) return hasPath(this._store, key);
-		return !isUndefined(this._store[(key as string)]);
+	public has(key: ListPath): boolean {
+		return hasPath(this._store.state, key);
 	}
 	
 	/**
@@ -69,9 +91,8 @@ export class Store {
 	 * @param key The name of the stored value to extract
 	 * @param defaultValue An optional default value to return if the given key was not found
 	 */
-	public get(key: string | Array<string>, defaultValue?: any): any {
-		if (isArray(key) || isString(key) && key.indexOf(".") !== -1) return getPath(this._store, key, defaultValue);
-		return isUndefined(this._store[(key as string)]) ? defaultValue : this._store[(key as string)];
+	public get(key: ListPath, defaultValue?: any): any {
+		return getPath(this._store.state, key, defaultValue);
 	}
 	
 	/**
@@ -80,7 +101,49 @@ export class Store {
 	 * @param value The value to set for the given key
 	 */
 	public set(key: string, value: any): Store {
-		Vue.set(this._store, key, value);
+		Vue.set(this._store.state, key, value);
+		return this;
+	}
+	
+	/**
+	 * An alias for bind()
+	 * @param key
+	 * @param callback
+	 * @see Store.bind()
+	 */
+	public watch(key: string, callback: StoreWatcher): Store {
+		return this.bind(key, callback);
+	}
+	
+	/**
+	 * Used to watch a certain store property for changes. Works in the same way your
+	 * normal vue watchers work, the callback receives two values, the first is the new the second the previous value.
+	 *
+	 * @param key The key of the property to watch
+	 * @param callback The callback to execute when a property changes
+	 */
+	public bind(key: string, callback: StoreWatcher): Store {
+		// Check if we have to watch for changes
+		if (!this._watched.has(key)) {
+			// Register a new watcher
+			this._watched.add(key);
+			this._store.$watch(() => this._store.state[key], (n, o) => {
+				this._emitter.emit("update", [n, o]);
+			});
+		}
+		// Add the callback as listener
+		this._emitter.bind("update", callback);
+		return this;
+	}
+	
+	/**
+	 * Used to unbind a previously bound callback from watching a property.
+	 * @param key The key of the property to unbind the given callback from
+	 * @param callback The callback to unbind from watching a property. If this is not given
+	 * all callbacks are removed for the given property
+	 */
+	public unbind(key: string, callback?: StoreWatcher): Store {
+		this._emitter.unbind("update", callback);
 		return this;
 	}
 	
@@ -89,7 +152,7 @@ export class Store {
 	 * Note: be careful with this or you can break things quite easily.
 	 */
 	public getRaw(): PlainObject {
-		return this._store;
+		return this._store.state;
 	}
 	
 }
