77267885ab640d82ee24d8d5a28e5f7605353e2e
Martin Neundorfer <m.neundorfer@labor.digital>
2020-06-17 20:02:03 +0200

    feat(Store): allow store watchers to accept vue watcher options

--- src/Core/Module/General/Store.ts
+++ src/Core/Module/General/Store.ts
@@ -16,7 +16,6 @@
  * Last modified: 2019.10.29 at 17:41
  */
 
-import {EventEmitter} from "@labor-digital/helferlein/lib/Events/EventEmitter";
 import {ListPath} from "@labor-digital/helferlein/lib/Interfaces/List";
 import {PlainObject} from "@labor-digital/helferlein/lib/Interfaces/PlainObject";
 import {forEach} from "@labor-digital/helferlein/lib/Lists/forEach";
@@ -25,7 +24,7 @@ import {hasPath} from "@labor-digital/helferlein/lib/Lists/Paths/hasPath";
 import {isFunction} from "@labor-digital/helferlein/lib/Types/isFunction";
 import {isPlainObject} from "@labor-digital/helferlein/lib/Types/isPlainObject";
 import {isUndefined} from "@labor-digital/helferlein/lib/Types/isUndefined";
-import Vue from "vue";
+import Vue, {WatchOptions} from "vue";
 
 export interface StoreWatcher {
 	(n: any, o: any): void
@@ -42,19 +41,14 @@ interface StoreVue extends Vue {
 export class Store {
 	
 	/**
-	 * The event emitter to perform the binding proxy with
-	 */
-	protected _emitter: EventEmitter;
-	
-	/**
 	 * The internal, reactive storage object
 	 */
 	protected _store: StoreVue;
 	
 	/**
-	 * The list of already watched properties inside the store
+	 * A list of callbacks used to destroy previously bound watchers
 	 */
-	protected _watched: Set<string>;
+	protected _unbinders: Map<Function, Function>;
 	
 	public constructor(internalInitialState: PlainObject, initialState ?: Function) {
 		// Use a fallback to create an internal state if we did not got one provided
@@ -73,9 +67,8 @@ export class Store {
 			data: {state: loadedState}
 		});
 		
-		// Create the event emitter instance and the list of watched properties
-		this._emitter = new EventEmitter(true);
-		this._watched = new Set<string>();
+		// Create the list of unbinders to destroy bindings
+		this._unbinders = new Map<Function, Function>();
 	}
 	
 	/**
@@ -87,7 +80,7 @@ export class Store {
 	}
 	
 	/**
-	 * Returns the value of a certain storage key or the default value if it does not exist
+	 * Returns the value of a certain storage key, or the default value if it does not exist
 	 * @param key The name of the stored value to extract
 	 * @param defaultValue An optional default value to return if the given key was not found
 	 */
@@ -109,10 +102,11 @@ export class Store {
 	 * An alias for bind()
 	 * @param key
 	 * @param callback
+	 * @param options
 	 * @see Store.bind()
 	 */
-	public watch(key: string, callback: StoreWatcher): Store {
-		return this.bind(key, callback);
+	public watch(key: string, callback: StoreWatcher, options?: WatchOptions): Store {
+		return this.bind(key, callback, options);
 	}
 	
 	/**
@@ -121,38 +115,41 @@ export class Store {
 	 *
 	 * @param key The key of the property to watch
 	 * @param callback The callback to execute when a property changes
+	 * @param options Allows setting additional options for your watcher.
 	 */
-	public bind(key: string, callback: StoreWatcher): Store {
-		// Check if we have to watch for changes
-		if (!this._watched.has(key)) {
-			// Register a new watcher
-			this._watched.add(key);
-			this._store.$watch(() => this._store.state[key], (n, o) => {
-				this._emitter.emit("update", [n, o]);
-			});
-		}
-		// Add the callback as listener
-		this._emitter.bind("update", callback);
+	public bind(key: string, callback: StoreWatcher, options?: WatchOptions): Store {
+		// Only bind watchers once
+		if (this._unbinders.has(callback)) this._unbinders.get(callback)();
+		
+		// Create new watcher
+		const unbinder = this._store.$watch("state." + key, callback, options);
+		this._unbinders.set(callback, unbinder);
 		return this;
 	}
 	
 	/**
 	 * Used to unbind a previously bound callback from watching a property.
 	 * @param key The key of the property to unbind the given callback from
-	 * @param callback The callback to unbind from watching a property. If this is not given
-	 * all callbacks are removed for the given property
+	 * @param callback The callback to unbind from watching a property.
 	 */
-	public unbind(key: string, callback?: StoreWatcher): Store {
-		this._emitter.unbind("update", callback);
+	public unbind(key: string, callback: StoreWatcher): Store {
+		if (this._unbinders.has(callback)) this._unbinders.get(callback)();
 		return this;
 	}
 	
 	/**
 	 * Returns the internal store object.
-	 * Note: be careful with this or you can break things quite easily.
+	 * Note: be careful with this, or you can break things quite easily.
 	 */
 	public getRaw(): PlainObject {
 		return this._store.state;
 	}
 	
+	/**
+	 * Returns the internal vue instance we use as reactive store
+	 * Note: be careful with this, or you can break things quite easily.
+	 */
+	public getRawVue(): Vue | any {
+		return this._store;
+	}
 }
